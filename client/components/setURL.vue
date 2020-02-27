dc569af702ac680f1885d2a385d583f73e2eabb6
sunikaaa <sin.seki.ara@gmail.com>
2020-02-28 01:00:48 +0900

    SCSS変数対応

--- client/components/setURL.vue
+++ client/components/setURL.vue
@@ -4,7 +4,7 @@
       <p>YouTube ライブ配信中のページURLを入力して下さい</p>
     </div>
     <div class="input-style">
-      <input type="text" placeholder=" URL"/>
+      <input type="text" placeholder=" URL" />
     </div>
     <button class="button-style">始める</button>
   </div>
@@ -12,41 +12,41 @@
 
 <script></script>
 
-<style scoped>
-  .set-url{
-    height: 100vh;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-weight: 400;
-    color: #707070;
-  }
-  .message{
-    font-size: calc(15px + 0.8vw);
-    margin: 0 10vw;
-  }
-  .input-style{
-    width: calc(40% + 15vh);
-    margin: 10vh 0;
-  }
-  .input-style input{
+<style lang="scss" scoped>
+.set-url {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-weight: 400;
+  color: $textColor;
+}
+.message {
+  font-size: $fontSizeML;
+  margin: 0 10vw;
+}
+.input-style {
+  width: calc(40% + 15vh);
+  margin: 10vh 0;
+  input {
     height: 2em;
     width: 100%;
-    font-size: calc(14px + 0.5vw);
-    border: 1px #707070 solid;
+    font-size: $fontSizeMM;
+    border: 1px $textColor solid;
     border-radius: 10px;
   }
-  .button-style{
-    width: 8em;
-    height: 2.5em;
-    display: inline-block;
-    font-size: calc(14px + 0.5vw);
-    background-color: #707070;
-    color: #ffffff;
-    border-style: none;
-    border-radius: 10px;
-    cursor: pointer;
-  }
+}
+.button-style {
+  width: 8em;
+  height: 2.5em;
+  display: inline-block;
+  font-size: $fontSizeMM;
+  background-color: $bottonColor;
+  color: #ffffff;
+  border-style: none;
+  border-radius: 10px;
+  cursor: pointer;
+}
 </style>
