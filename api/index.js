8514c31e882e7631d780df2970fd6e4a21674835
sunikaaa <sin.seki.ara@gmail.com>
2020-02-28 18:45:04 +0900

    パッケージ追加

--- api/index.js
+++ api/index.js
@@ -1,5 +1,40 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const redis = require("redis");
+
+// const connectDB = mysql.createConnection({
+//   host: "mysql",
+//   port: 3306,
+//   user: "root",
+//   password: "rootpass"
+// });
+
+// connectDB.connect(err => {
+//   if (err) throw err;
+//   console.log("Connected");
+// });
+
+// const config = {
+//   host: "redis",
+//   port: 6379
+// };
+
+// const client = redis.createClient(config);
+// // データの登録
+// client.set("key", "value");
+// // データの取得と表示
+// client.get("key", (err, reply) => {
+//   console.log(reply);
+// });
+// // データの削除
+// client.del("key");
+// // 削除されているか確認
+// client.get("key", (err, reply) => {
+//   console.log(reply);
+// });
+// // 切断
+// client.quit();
 
 app.get("/", function(req, res) {
   res.send("HelloWorld");
