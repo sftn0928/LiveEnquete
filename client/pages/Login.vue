351bb362676a10e33cc9e18dd411462d7af971e2
Def-aki0530 <systemerrrornotfound@gmail.com>
2020-03-02 23:15:00 +0900

    n

--- client/pages/Login.vue
+++ client/pages/Login.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <div class="login-page">
         <div class="login-page-contents">
             <p>メールアドレス</p>
@@ -58,37 +57,3 @@ export default {};
     }
 }
 </style>
-=======
-  <div>
-    <div>メールアドレス:<input type="email" v-model="email" /></div>
-    <div>パスワード:<input type="password" v-model="pass" /></div>
-    <div><input type="submit" value="ログイン" @click="submitToServer" /></div>
-  </div>
-</template>
-<script>
-// import axios from "axios";
-import myAxios from "~/plugins/myAxios";
-
-export default {
-  data() {
-    return {
-      email: "",
-      pass: ""
-    };
-  },
-  methods: {
-    submitToServer() {
-      let obj = {
-        email: this.email,
-        pass: this.pass
-      };
-      console.log(obj);
-      myAxios.post("/api/user", obj);
-
-      console.log("axios");
-    }
-  }
-};
-</script>
-<style scoped></style>
->>>>>>> feature/userCreate
