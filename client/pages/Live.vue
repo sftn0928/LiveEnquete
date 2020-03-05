caf51d95e4f168cf047743eb6b5fb1b49196f51f
sunikaaa <sin.seki.ara@gmail.com>
2020-03-05 14:41:17 +0000

    コンポーネントの場所変更

--- client/pages/Live.vue
+++ client/pages/Live.vue
@@ -3,25 +3,27 @@
     <button @click="isCollect = !isCollect">StartCollect! 10秒</button>
     <button @click="isSetURL = !isSetURL">setURL</button>
     <setURLComponent v-if="isSetURL === false" />
-    <collectComponent v-if="isCollect === false" />
-    <resultComponent v-if="isCollect" />
+    <collectComponent v-if="isCollect === false" :socket="socket" />
   </div>
 </template>
 <script>
 import collectComponent from "~/components/collectChat";
-import resultComponent from "~/components/result";
 import setURLComponent from "~/components/setURL";
+import io from "socket.io-client";
 export default {
   components: {
     collectComponent,
-    resultComponent,
     setURLComponent
   },
   data() {
     return {
       isCollect: false,
-      isSetURL: false
+      isSetURL: false,
+      socket: ""
     };
+  },
+  mounted() {
+    this.socket = io();
   }
 };
 </script>
